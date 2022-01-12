d4bdb033b89a33671bd787cb31cdc951ab312584
lijinling <1424518547@qq.com>
2022-01-12 15:57:07 +0800

    add .

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          github pages 个人网站搭建
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://1424518547.github.io/github-pages-demo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github pages
+        </a>
+        <a
+          className="App-link"
+          href="https://1424518547.github.io/github-pages-demo/deploy-react-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          在GitHub Pages中快速部署React项目
         </a>
       </header>
     </div>
